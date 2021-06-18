e5aea3e633e19a23e06e002d5ece3e668d6716a0
Alexander Gängel <alexander.gaengel@ionos.com>
2021-06-18 16:24:05 +0200

    format logo and footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -14,39 +14,11 @@ export default {
 <template>
   <div
     v-if="title || social"
-    class="
-      flex
-      absolute
-      w-full
-      bottom-0
-      left-0
-      py-0.5
-      px-4
-      text-sm
-    "
+    class="flex absolute w-full bottom-0 right-2 py-0.5 px-4 text-sm"
   >
-    <div class="w-1/3 text-left flex justify-start items-center">
-      
-    </div>
-    <div class="w-1/3 flex justify-center">
-     <!-- <div
-        v-for="type in social"
-        :key="type.type"
-        class="flex justify-center items-center ml-4"
-      >
-        <a
-          :href="'https://github.com/' + type.username"
-          target="_blank"
-          alt="GitHub"
-          class="icon-btn !border-none !hover:text-white"
-        >
-          <LogoIcon :type="type.type" />
-        </a>
-      </div> -->
-    </div>
-    <div class="w-1/3 flex justify-end">
+    <div class="w-full flex justify-end">
       <a href="https://ionos.com" target="_blank">{{ title }}</a>
-      <mdi:minus class="fill-current text-sm"/>
+      <mdi:minus class="fill-current text-sm" />
       <a href="https://www.ionos.de/impressum" target="_blank">Imprint</a>
     </div>
   </div>
